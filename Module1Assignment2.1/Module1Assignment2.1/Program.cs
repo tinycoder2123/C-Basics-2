22f14b0b50bcc4d5ce5ad2f3afda67e55845ee98
tinycoder2123 <70987474+tinycoder2123@users.noreply.github.com>
2021-06-17 13:13:16 -0600

    console application that asks the user for two numbers
    Create a console application that asks the user for two numbers in the range 0-255 and then divides the first number by the second:
    
    Enter a number between 0 and 255: 100
    
    Enter another number between 0 and 255: 8
    
    100 divided by 8 is 12
    
    Enter a number between 0 and 255: apples
    
    Enter another number between 0 and 255: bananas
    
    FormatException: Input string was not in a correct format.

---
+++ Module1Assignment2.1/Module1Assignment2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Module1Assignment2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 0 and 255: ");
+            int number1;
+            int number2;
+            int result;
+
+            if (!int.TryParse(Console.ReadLine(), out number1))
+            {
+                Console.WriteLine("FormatException: Input string was not in a correct format.");
+            }
+            Console.Write("Enter another number between 0 and 255: ");
+            if (!int.TryParse(Console.ReadLine(), out number2))
+            {
+                Console.WriteLine("FormatException: Input string was not in a correct format.");
+            }
+            result = number1 / number2;
+            Console.WriteLine("{0} divided by {1} is {2}", number1, number2, result);
+        }
+    }
+}
